ddf1cf140b3492e08b04f929a276079e53e9f210
ximias <alexx4387@gmail.com>
2018-02-10 13:54:18 +0100

    Added the event condition functionality

---
+++ src/net/ximias/psEventHandlers/Condition.java
@@ -0,0 +1,21 @@
+package net.ximias.psEventHandlers;
+
+import java.util.function.BiPredicate;
+
+public enum Condition {
+	EQUALS((a,b)->a.equals(b)),
+	NOT_EQUALS((a,b)->!a.equals(b)),
+	GREATER((a,b)->Integer.valueOf(a)>Integer.valueOf(b)),
+	LESS((a,b)->Integer.valueOf(a)<Integer.valueOf(b)),
+	GREATER_OR_EQUALS((a,b)->Integer.valueOf(a)>=Integer.valueOf(b)),
+	LESS_OR_EQUALS((a,b)->Integer.valueOf(a)<=Integer.valueOf(b));
+	private final BiPredicate<String, String> predicate;
+	Condition(BiPredicate<String, String> predicate){
+		this.predicate = predicate;
+	}
+	
+	boolean eval(String a, String b){
+		return predicate.test(a,b);
+	}
+	
+}
--- src/net/ximias/psEventHandlers/EventCondition.java
+++ src/net/ximias/psEventHandlers/EventCondition.java
@@ -1,5 +1,11 @@
 package net.ximias.psEventHandlers;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+
 public class EventCondition {
 	
 	/*
@@ -16,4 +22,33 @@ public class EventCondition {
 	Process payload
 	Return true or false
 	 */
+	ArrayList<ConditionData> data;
+	
+	Condition condition;
+	
+	public EventCondition(ArrayList<ConditionData> data, Condition condition) {
+		this.data = data;
+		this.condition = condition;
+	}
+	
+	public boolean evaluate(JSONObject payload){
+			for (int i = 0; i < data.size()-1; i++) {
+				try{
+					if (!condition.eval(data.get(i).get(payload),data.get(i+1).get(payload))) return false;
+				}catch (NumberFormatException e){
+					System.out.println("Letters can not be compared using "+condition.name());
+					System.out.println("comparison was: "+data.get(i).get(payload)+" "+condition.name()+" "+data.get(i+1).get(payload));
+					return false;
+				}
+			}
+			return true;
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<ConditionData> data = new ArrayList<>();
+		data.add(new ConstantData("a",ConditionDataType.EVENT_VARIABLE));
+		data.add(new ConstantData("1",ConditionDataType.CONSTANT_DATA));
+		EventCondition econ = new EventCondition(data, Condition.GREATER_OR_EQUALS);
+		System.out.println(econ.evaluate(new JSONObject("{\"a\":\"1\"}")));
+	}
 }
